610b5cf7e32d6080ee38ba394a575ea461564dbd
rdprod <rdvorin55@gmail.com>
2019-03-22 08:54:06 +0000

    обновлены swap.c и revert_string.c, добавлены libshar.so, libstat.o и другие изменения

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,11 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	for ( int i = 0; i < strlen(str)/2; i++ )
+	{
+	    char buff = str[i];
+	    str[i] = str[strlen(str) - i - 1];
+	    str[strlen(str) - i - 1] = buff;
+	}
 }
 
